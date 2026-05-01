ae4107ca33c0cc3c248c80fd7dda51d1746e333c
shantunu007 <chakmashantunu.web@gmail.com>
2026-05-01 22:08:53 +0600

    implement nested routing with layout and outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 
 
 import { RouterProvider } from "react-router/dom";
 import { createBrowserRouter } from 'react-router';
+import Root from './components/Root/Root';
+import Home from './components/Home/Home';
+import Mobiles from './components/Mobiles/Mobiles';
+import Laptops from './components/Laptops/Laptops';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello boss!</div>
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: "mobiles", Component: Mobiles},
+      {path: "laptops", Component: Laptops}
+    ]
+  },
+  {
+    path: "about",
+    element: <div>about page</div>
+  },
+  {
+    path: 'app',
+    Component: App
   }
 ])
 

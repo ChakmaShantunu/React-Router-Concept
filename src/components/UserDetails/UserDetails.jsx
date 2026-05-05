f3c643e5c2f72b7dfcee0a8b1c3541a9ab7ad344
shantunu007 <chakmashantunu.web@gmail.com>
2026-05-05 22:28:56 +0600

    implement user details page with dynamic loader and data rendering

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UserDetails = () => {
+
+    const user = useLoaderData();
+    console.log(user);
+    const { website, username } = user;
     return (
         <div>
             <h3>User details here</h3>
+            <h2>Username: {username}</h2>
+            <p>Website: {website}</p>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -40,6 +40,7 @@ const router = createBrowserRouter([
       },
       {
         path: "users/:userId",
+        loader: ({ params }) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
         Component: UserDetails
       },
       {

aaaeefdbeb4784ae4fc93b927ecd77dcc7b92b61
shantunu007 <chakmashantunu.web@gmail.com>
2026-05-07 22:38:26 +0600

    implement navigation handlers with useNavigate in post components

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 
 const Post = ({ post }) => {
 
-
     const { id, title, body } = post;
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('/laptops')
+    }
     return (
         <div>
             <h3>Title: {title}</h3>
             <Link to={`/posts/${id}`}><button>Show Details</button></Link>
+            <button onClick={handleNavigate}>Details of: {id}</button>
         </div>
     );
 };
--- src/components/PostDetail/PostDetail.jsx
+++ src/components/PostDetail/PostDetail.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetail = () => {
 
     const postDetail = useLoaderData();
     console.log(postDetail);
+
+    let navigate = useNavigate();
     return (
         <div>
             <h3>{postDetail.body}</h3>
+            <button onClick={() => navigate(-1)}>Go Back</button>
         </div>
     );
 };

8225e2edb1ea9157c6b6a90f455fb4cf7bbb36b5
shantunu007 <chakmashantunu.web@gmail.com>
2026-05-09 22:20:31 +0600

    Load user details dynamically with Suspense and use() hook

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Link } from 'react-router';
+import UserDetails2 from '../UserDetails2/UserDetails2';
 
-
-
-
-const userStyle = {
-    border: '2px solid yellow',
-    borderRadiun: '20px',
-    padding: '10px',
-    margin: '10px'
-}
 
 const User = ({ user }) => {
+
+    const userStyle = {
+        border: '2px solid yellow',
+        borderRadiun: '20px',
+        padding: '10px',
+        margin: '10px'
+    }
     const { name, email, phone, id } = user;
     console.log(user);
 
     const [showInfo, setShowInfo] = useState(false)
+
+    const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json());
+
     return (
         <div style={userStyle}>
             <h3>{name}</h3>
@@ -23,6 +25,12 @@ const User = ({ user }) => {
             <p>Phone: {phone}</p>
             <Link to={`/users/${id}`}>Show details</Link>
             <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? 'hide' : 'Show'} info</button>
+
+            {
+                showInfo && <Suspense fallback={<span>Loading...</span>}>
+                    <UserDetails2 userPromise={userPromise}></UserDetails2>
+                </Suspense>
+            }
         </div>
     );
 };
